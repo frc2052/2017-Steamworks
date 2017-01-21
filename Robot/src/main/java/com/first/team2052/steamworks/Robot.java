7b5a7217575cff0a292932705019612359684c1f
Adam8234 <Acorpstein8234@gmail.com>
2017-01-21 09:43:39 -0600

    Use static Talon libs and update path loader

--- Robot/src/main/java/com/first/team2052/steamworks/Robot.java
+++ Robot/src/main/java/com/first/team2052/steamworks/Robot.java
@@ -2,6 +2,7 @@ package com.first.team2052.steamworks;
 
 import com.first.team2052.lib.ControlLoop;
 import com.first.team2052.lib.Loopable;
+import com.first.team2052.steamworks.auto.AutoPaths;
 import com.first.team2052.steamworks.subsystems.DriveTrain;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
@@ -11,16 +12,11 @@ public class Robot extends IterativeRobot {
     private ControlLoop controlLoop = new ControlLoop(Constants.kControlLoopPeriod);
     DriveTrain driveTrain = new DriveTrain();
     Joystick joystick0 = new Joystick(0);
+    AutoPaths autoPaths = new AutoPaths();
 
     @Override
     public void robotInit() {
         controlLoop.addLoopable(driveTrain);
-        controlLoop.addLoopable(new Loopable() {
-            @Override
-            public void update() {
-                System.out.println("Hello 2017");
-            }
-        });
     }
 
     @Override
--- Robot/src/main/java/com/first/team2052/steamworks/auto/AutoPaths.java
+++ Robot/src/main/java/com/first/team2052/steamworks/auto/AutoPaths.java
@@ -1,21 +1,17 @@
 package com.first.team2052.steamworks.auto;
 
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.HashMap;
-
 import com.first.team2052.trajectory.common.Path;
 import com.first.team2052.trajectory.common.io.TextFileDeserializer;
 import com.google.common.base.Charsets;
 import com.google.common.base.Optional;
 import com.google.common.collect.Maps;
-import com.google.common.io.Files;
+import com.google.common.io.Resources;
+
+import java.util.HashMap;
 
 public class AutoPaths {
-    String[] pathsNames = new String[] { "LowBarHighGoalPath", "TestPath", "Position2ToCenterPath", "Position2bToCenterPath", "Position3ToCenterPath", "Position4ToCenterPath", "Position5ToCenterPath",
-            "Position5bToCenterPath", "LowBarCrossBackPath" };
+    String[] pathsNames = new String[]{"TestPath"};
     HashMap<String, Path> paths = Maps.newHashMap();
 
     public AutoPaths() {
@@ -30,12 +26,9 @@ public class AutoPaths {
             String content = null;
 
             try {
-                File file = new File(String.format("/home/lvuser/paths/%s.txt", pathName));
-                content = Files.toString(file, Charsets.UTF_8);
-            } catch (FileNotFoundException exception) {
+                content = Resources.toString(Resources.getResource("paths/" + pathName + ".txt"), Charsets.UTF_8);
+            } catch (Exception exception) {
                 System.out.println("Error: Couldn't find " + pathName + " path not loaded");
-            } catch (IOException e) {
-                e.printStackTrace();
             }
 
             if (content == null) {
