089c355c6849e6fe87a3d49cf0ca3ee47ceda594
student <controls@team2052.com>
2017-01-28 11:09:47 -0600

    Added a reference to the intake and gear intake in Robot

--- Robot/src/main/java/com/first/team2052/steamworks/Robot.java
+++ Robot/src/main/java/com/first/team2052/steamworks/Robot.java
@@ -4,8 +4,10 @@ import com.first.team2052.lib.ControlLoop;
 import com.first.team2052.lib.Loopable;
 import com.first.team2052.steamworks.auto.AutoPaths;
 import com.first.team2052.steamworks.subsystems.DriveTrain;
+import com.first.team2052.steamworks.subsystems.Intake;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Solenoid;
 
 
 public class Robot extends IterativeRobot {
@@ -13,6 +15,9 @@ public class Robot extends IterativeRobot {
     DriveTrain driveTrain = new DriveTrain();
     Joystick joystick0 = new Joystick(0);
     AutoPaths autoPaths = new AutoPaths();
+    Intake intake = new Intake();
+
+    public static double INTAKE_VELOCITY = 0.8;//changable variable
 
     @Override
     public void robotInit() {
@@ -43,11 +48,30 @@ public class Robot extends IterativeRobot {
 
     @Override
     public void testPeriodic() {
+        //gear intake control
+        if(joystick0.getRawButton(1)){
+            gearToggle(true);
+        } else {
+            gearToggle(false);
+        }
+
+        //fuel intake control
+        intake.setIntakeVelocity(INTAKE_VELOCITY, joystick0.getRawButton(3));
     }
 
     @Override
     public void disabledInit() {
         controlLoop.stop();
     }
+
+
+    //creates solenoids for gear
+    Solenoid gearIn = new Solenoid(0); //whats the channels for the gear motors?
+    Solenoid gearOut = new Solenoid(1);
+    public void gearToggle(boolean button){ //if button is pressed, toggle gear holder
+            gearIn.set(button);
+            gearOut.set(!button);
+    }
 }
+
 
