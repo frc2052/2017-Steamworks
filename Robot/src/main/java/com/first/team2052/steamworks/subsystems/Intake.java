b83f94a351fd40c2a6f2efd56c07d85db27c7da3
student <controls@team2052.com>
2017-01-28 10:25:17 -0600

    added a velocity variable that can easily be changed

--- Robot/src/main/java/com/first/team2052/steamworks/subsystems/Intake.java
+++ Robot/src/main/java/com/first/team2052/steamworks/subsystems/Intake.java
@@ -1,6 +1,7 @@
-package com.first.team2052.steamworks;
+package com.first.team2052.steamworks.subsystems;
 
 import com.ctre.CANTalon;
+import com.first.team2052.steamworks.Constants;
 
 /**
  * This class congregates the functionality of the intake apparatus; this is just the fuel intake.
@@ -11,14 +12,12 @@ public class Intake {
 
     /**
      * This function takes in a value from the joystick and if the appropriate button (for enabling the intake) is pressed,
-     * then the speed of the intake will optimize, based on the Robot's Velocity.
-     * @param joystickValue
+     * @param inVelocity
      * @param button
      */
-    public void setIntakeVelocity(double joystickValue, boolean button){
+    public void setIntakeVelocity(double inVelocity, boolean button){
         if(button) {//press the intake button
-            double speed = -0.904 * (Math.abs(joystickValue)) + .95;//-0.90476190476
-            intakeMotor.set(speed);
+            intakeMotor.set(inVelocity);
         }
     }
 
